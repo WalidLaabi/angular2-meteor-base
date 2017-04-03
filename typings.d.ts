1e476d34f85951ae23160560fa570f6c3072c9bc
LAABI <walid.laabi@capgemini.com>
2017-04-03 12:19:41 +0100

    pagination + insertion no complite

--- typings.d.ts
+++ typings.d.ts
@@ -28,6 +28,21 @@ declare module "*.sass" {
   const style: string;
   export default style;
 }
+ 
+declare module 'meteor/tmeasday:publish-counts' {
+  import { Mongo } from 'meteor/mongo';
+ 
+  interface CountsObject {
+    get(publicationName: string): number;
+    publish(context: any, publicationName: string, cursor: Mongo.Cursor, options: any): number;
+  }
+ 
+  export const Counts: CountsObject;
+}
+
+declare var Fake: {
+    sentence(words: number): string;
+}
 
 declare module "meteor/hwillson:stub-collections" {
   import { Mongo } from "meteor/mongo";
